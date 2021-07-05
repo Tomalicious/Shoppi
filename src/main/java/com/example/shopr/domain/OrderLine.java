26ff74d61b2a6c9a93f94cca36bbd7d0dcc20307
Tom Van Laeken <tototonique@gmail.com>
2021-07-05 22:25:48 +0200

    Committed

---
+++ src/main/java/com/example/shopr/domain/OrderLine.java
@@ -0,0 +1,37 @@
+package com.example.shopr.domain;
+
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import lombok.experimental.SuperBuilder;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//
+//@Data
+//@Entity
+//@SuperBuilder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class OrderLine {
+//
+//
+//    @Id
+//    @GeneratedValue
+//    private Long id;
+//
+//    private BookFiction bookFiction;
+//    private BookNonFiction bookNonFiction;
+//    private Game game;
+//    private Lp article;
+//
+//    private int quantity;
+//
+//    private double currentPrice;
+//
+//
+//
+//}
