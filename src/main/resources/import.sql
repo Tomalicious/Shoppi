4f281c0906cb9ec570c4aa32da97fabb5173daf0
Tom Van Laeken <tototonique@gmail.com>
2021-08-10 10:34:07 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,8 @@
 INSERT INTO book ( type , price ,publisher, supplier_id , title , author , isbn , pages ,  book_genre , description , subject , stock) values ( 'FICTION' , 10.99 , 'Tommeke' ,3 , 'Twilight' , 'JKJK' , '978-90-1234-567-8' , 150 , 'FANTASY' , 'Once upon a time..' , null , 100),( 'NON FICTION' , 10.99 , 'Tommeke' ,3 , 'Der Untergang' , 'JKJK' , '978-90-1234-567-9' , 150 , 'FANTASY' , 'Once upon a time..' , 'OTHER' , 110)
-INSERT INTO game(`type`,`price`, `supplier_id`, `title`, `game_genre`, `minimum_age`,`publisher` ,`stock`)VALUES( 'GAME', 59.99 , 3 , 'World On Fire' , 'MMORPG' , 12 , 'Blizzard', 100)
-INSERT INTO users(id , user , password , authorization ) VALUES (1 , 'employee' , 'employee' , 'EMPLOYEE'), (2, 'Tomalicious' , 't' , 'CLIENT')
-INSERT INTO `Shopr`.`supplier` (`id`) VALUES ('1');
-INSERT INTO `Shopr`.`supplier` (`id`) VALUES ('2');
-INSERT INTO `Shopr`.`supplier` (`id`) VALUES ('3');
-INSERT INTO `Shopr`.`supplier` (`id`) VALUES ('4');
-INSERT INTO `Shopr`.`supplier` (`id`) VALUES ('5');
+-- INSERT INTO game(`type`,`price`, `supplier_id`, `title`, `game_genre`, `minimum_age`,`publisher` ,`stock`)VALUES( 'GAME', 59.99 , 3 , 'World On Fire' , 'MMORPG' , 12 , 'Blizzard', 100)
+-- INSERT INTO users(id , user , password , authorization ) VALUES (1 , 'employee' , 'employee' , 'EMPLOYEE'), (2, 'Tomalicious' , 't' , 'CLIENT')
+-- INSERT INTO `Shopr`.`supplier` (`id`) VALUES ('1');
+-- INSERT INTO `Shopr`.`supplier` (`id`) VALUES ('2');
+-- INSERT INTO `Shopr`.`supplier` (`id`) VALUES ('3');
+-- INSERT INTO `Shopr`.`supplier` (`id`) VALUES ('4');
+-- INSERT INTO `Shopr`.`supplier` (`id`) VALUES ('5');
