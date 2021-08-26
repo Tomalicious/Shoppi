6ec806dfdaaae4c6e5272a503e93bb056396c5dd
Tom Van Laeken <tototonique@gmail.com>
2021-08-26 11:59:25 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/example/shopr/services/DetailService.java
+++ src/main/java/com/example/shopr/services/DetailService.java
@@ -5,6 +5,7 @@ import com.example.shopr.domain.*;
 import com.example.shopr.repositories.ArticleRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 @Service
 public class DetailService {
     @Autowired
