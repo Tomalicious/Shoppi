f5264ce8604a4246ae52ae3c48196865d8bb4cc6
Tom Van Laeken <tototonique@gmail.com>
2021-08-26 12:01:46 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/com/example/shopr/interfaces/ArticleRepository.java
@@ -0,0 +1,10 @@
+package com.example.shopr.interfaces;
+
+import com.example.shopr.domain.Article;
+import com.example.shopr.repositories.Repository;
+
+public interface ArticleRepository extends Repository<Article , Long> {
+
+
+
+}
---
+++ src/main/java/com/example/shopr/interfaces/LpRepository.java
@@ -0,0 +1,9 @@
+package com.example.shopr.interfaces;
+
+import com.example.shopr.domain.Lp;
+import com.example.shopr.repositories.Repository;
+
+public interface LpRepository extends Repository<Lp, Long> {
+
+
+}
